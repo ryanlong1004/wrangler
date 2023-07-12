08fbeed952e6829001b4d04a4637606bb02b0101
Ryan Long <ryanlong1004@gmail.com>
2023-07-12 20:38:19 +0000

    Log to stdout, not file

--- src/__main__.py
+++ src/__main__.py
@@ -13,8 +13,7 @@ from src.script import Script, ScriptNotFound, find_script
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s %(levelname)s %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    # filename="wrangler.log",
+    datefmt="%Y-%m-%d %H:%M:%S"
 )
 
 logger = logging.getLogger(__name__)
